c3b81c3c70a447a145b4799111318eb97631d1eb
Ngochb2809 <ngochbgbh211287@fpt.edu.vn>
2023-02-22 15:43:59 +0700

    Add project files.

---
+++ Controllers/CartController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Book_Store.Models;
+using Book_Store.Data;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+
+namespace Book_Store.Controllers
+{
+    public class CartController : Controller
+    {
+        private ApplicationDbContext _db;
+        // GET: Shop
+        public CartController(ApplicationDbContext db) 
+        {
+            this._db = db;
+        }
+        [Authorize(Roles = "User")]
+        public IActionResult Index()
+        {
+            var _book = getAllBook();
+            ViewBag.book = _book;
+            return View();
+        }
+        [Authorize(Roles = "User")]
+        public List<Book> getAllBook()
+        {
+            return _db.Book.ToList();
+        }
+        [Authorize(Roles = "User")]
+        public Book getDetailBook(int id)
+        {
+            var book = _db.Book.Find(id);
+            return book;
+        }
+        [Authorize(Roles = "User")]
+        public IActionResult addCart(int id)
+        {
+            var cart = HttpContext.Session.GetString("cart");//get key cart
+            if (cart == null)
+            {
+                var book = getDetailBook(id);
+                List<Cart> listCart = new List<Cart>()
+               {
+                   new Cart
+                   {
+                       Book = book,
+                       Quantity = 1
+                   }
+               };
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(listCart));
+
+            }
+            else
+            {
+                List<Cart> dataCart = JsonConvert.DeserializeObject<List<Cart>>(cart);
+                bool check = true;
+                for (int i = 0; i < dataCart.Count; i++)
+                {
+                    if (dataCart[i].Book.Id == id)
+                    {
+                        dataCart[i].Quantity++;
+                        check = false;
+                    }
+                }
+                if (check)
+                {
+                    dataCart.Add(new Cart
+                    {
+                        Book = getDetailBook(id),
+                        Quantity = 1
+                    });
+                }
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(dataCart));
+                // var cart2 = HttpContext.Session.GetString("cart");//get key cart
+                //  return Json(cart2);
+            }
+
+            return RedirectToAction(nameof(Index));
+
+        }
+        [Authorize(Roles = "User")]
+        public IActionResult ListCart()
+        {
+            var cart = HttpContext.Session.GetString("cart");//get key cart
+            if (cart != null)
+            {
+                List<Cart> dataCart = JsonConvert.DeserializeObject<List<Cart>>(cart);
+                if (dataCart.Count > 0)
+                {
+                    ViewBag.carts = dataCart;
+                    return View();
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        [Authorize(Roles = "User")]
+        [HttpPost]
+        public IActionResult updateCart(int id, int quantity)
+        {
+            var cart = HttpContext.Session.GetString("cart");
+            if (cart != null)
+            {
+                List<Cart> dataCart = JsonConvert.DeserializeObject<List<Cart>>(cart);
+                if (quantity > 0)
+                {
+                    for (int i = 0; i < dataCart.Count; i++)
+                    {
+                        if (dataCart[i].Book.Id == id)
+                        {
+                            dataCart[i].Quantity = quantity;
+                        }
+                    }
+
+
+                    HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(dataCart));
+                }
+                var cart2 = HttpContext.Session.GetString("cart");
+                return Ok(quantity);
+            }
+            return BadRequest();
+
+        }
+        [Authorize(Roles = "User")]
+        public IActionResult deleteCart(int id)
+        {
+            var cart = HttpContext.Session.GetString("cart");
+            if (cart != null)
+            {
+                List<Cart> dataCart = JsonConvert.DeserializeObject<List<Cart>>(cart);
+
+                for (int i = 0; i < dataCart.Count; i++)
+                {
+                    if (dataCart[i].Book.Id == id)
+                    {
+                        dataCart.RemoveAt(i);
+                    }
+                }
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(dataCart));
+                return RedirectToAction(nameof(ListCart));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
