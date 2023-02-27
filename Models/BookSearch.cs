527ddf032506dd5bd9f334e2251fdae50a73fcf9
Ngochb2809 <ngochbgbh211287@fpt.edu.vn>
2023-02-27 16:03:16 +0700

    commit 1

---
+++ Models/BookSearch.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Book_Store.Models
+{
+    public class BookSearch
+    {
+        public List<Book> Books { get; set; }
+        public SelectList? Orderss { get; set; }
+        public string? BooksSearch { get; set; }
+    }
+}
